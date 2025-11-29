e895378dc792ed2dc372e6032bcd311d9cc98d15
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-11-29 23:39:17 +0900

    feat: HTMLで選択されたモードを取得する処理を追加

---
+++ 本番用ファイル/theme-toggle-v2.js
@@ -0,0 +1,9 @@
+{
+  /**
+   * HTMLで選択されたモードを取得する処理
+   */
+  function getThemeMode() {
+    const currentThemeElement = document.getElementById("theme-toggle");
+    console.log(currentThemeElement.checked);
+  }
+}
