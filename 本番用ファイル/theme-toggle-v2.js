4d89365ff5d9d00145b04a87eaeafe02b0c03cfb
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-12-01 00:10:34 +0900

    refactor(theme-toggle-v2): コードを簡素化し、初期モード設定を削除

--- 本番用ファイル/theme-toggle-v2.js
+++ 本番用ファイル/theme-toggle-v2.js
@@ -1,58 +1,22 @@
 {
-  /**
-   * 初期モードをダークに設定する
-   */
+  const currentThemeElement = document.getElementById("theme-toggle");
 
-  /**
-   * HTMLで選択されたモードを取得する処理
-   */
-  function getToggleElement() {
-    const el = document.getElementById("theme-toggle");
-    console.log(el.checked);
-    return el;
-  }
   /**
    * ボタンが押されたかどうかを確認できる処理
    * 引数はel.checked→trueだったらtrueだよ，
    * もしフォルスならapplyTheme("light"), trueならapplyTheme("dark")
    */
   function handleToggle() {
-    const el = getToggleElement();
-    let isDarkMode = el.checked;
-    isDarkMode === true ? applyTheme("dark") : applyTheme("light");
+    applyTheme(currentThemeElement.checked ? "dark" : "light");
   }
   /**
    * テーマを適用するための処理
-   * @param {string} theme
+   * @param {string} nextTheme
    */
+  const applyTheme = (nextTheme) => {
+    document.documentElement.setAttribute("data-bs-theme", nextTheme);
+  };
 
-  function applyTheme(theme) {
-    document.documentElement.setAttribute("data-bs-theme", theme);
-  }
-
-  /**
-   * モードがダークならライトにする
-   * 戻り値は反映されたHTML要素
-   */
-  function handleLightTheme() {
-    let theme = document.documentElement.getAttribute("data-bs-theme");
-    if (theme === "dark") {
-      theme = "light";
-      document.documentElement.setAttribute("data-bs-theme", "light");
-    }
-    console.log("theme:", theme);
-  }
-
-  function handleDarkTheme() {
-    let theme = document.documentElement.getAttribute("data-bs-theme");
-    if (theme === "light") {
-      theme = "dark";
-      document.documentElement.setAttribute("data-bs-theme", "dark");
-    }
-    console.log("theme:", theme);
-  }
-
-  const currentThemeElement = getToggleElement();
   currentThemeElement.addEventListener("change", handleToggle);
 
   /**
