a43c8555683f073f6f233ff57316acd1c1ad4478
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-12-01 00:11:52 +0900

    refactor(theme-toggle-v2): 不要なブロックを削除し、コードを整理

--- 本番用ファイル/theme-toggle-v2.js
+++ 本番用ファイル/theme-toggle-v2.js
@@ -1,27 +1,25 @@
-{
-  const currentThemeElement = document.getElementById("theme-toggle");
+const currentThemeElement = document.getElementById("theme-toggle");
 
-  /**
-   * ボタンが押されたかどうかを確認できる処理
-   * 引数はel.checked→trueだったらtrueだよ，
-   * もしフォルスならapplyTheme("light"), trueならapplyTheme("dark")
-   */
-  function handleToggle() {
-    applyTheme(currentThemeElement.checked ? "dark" : "light");
-  }
-  /**
-   * テーマを適用するための処理
-   * @param {string} nextTheme
-   */
-  const applyTheme = (nextTheme) => {
-    document.documentElement.setAttribute("data-bs-theme", nextTheme);
-  };
-
-  currentThemeElement.addEventListener("change", handleToggle);
-
-  /**
-   * 選択されたモードを記録する
-   * TODO ローカルストレージ保存機能を実装
-   */
-  function saveSelectTheme() {}
+/**
+ * ボタンが押されたかどうかを確認できる処理
+ * 引数はel.checked→trueだったらtrueだよ，
+ * もしフォルスならapplyTheme("light"), trueならapplyTheme("dark")
+ */
+function handleToggle() {
+  applyTheme(currentThemeElement.checked ? "dark" : "light");
 }
+/**
+ * テーマを適用するための処理
+ * @param {string} nextTheme
+ */
+const applyTheme = (nextTheme) => {
+  document.documentElement.setAttribute("data-bs-theme", nextTheme);
+};
+
+currentThemeElement.addEventListener("change", handleToggle);
+
+/**
+ * 選択されたモードを記録する
+ * TODO ローカルストレージ保存機能を実装
+ */
+function saveSelectTheme() {}
