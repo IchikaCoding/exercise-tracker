ddf7705b01acc7e1de56077233af6a2f8c4cc1bc
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-12-01 22:27:10 +0900

    fix(theme-toggle-v2): 変数名を修正。ローカルストレージ保存のための処理を追加

--- 本番用ファイル/theme-toggle-v2.js
+++ 本番用ファイル/theme-toggle-v2.js
@@ -1,12 +1,12 @@
-const currentThemeElement = document.getElementById("theme-toggle");
-
+const themeToggle = document.getElementById("theme-toggle");
+const TOGGLE_THEME_STORAGE_KEY = "toggleThemeEntry";
 /**
  * ボタンが押されたかどうかを確認できる処理
  * 引数はel.checked→trueだったらtrueだよ，
  * もしフォルスならapplyTheme("light"), trueならapplyTheme("dark")
  */
 function handleToggle() {
-  applyTheme(currentThemeElement.checked ? "dark" : "light");
+  applyTheme(themeToggle.checked ? "dark" : "light");
 }
 /**
  * テーマを適用するための処理
@@ -16,10 +16,27 @@ const applyTheme = (nextTheme) => {
   document.documentElement.setAttribute("data-bs-theme", nextTheme);
 };
 
-currentThemeElement.addEventListener("change", handleToggle);
+themeToggle.addEventListener("change", handleToggle);
 
 /**
- * 選択されたモードを記録する
- * TODO ローカルストレージ保存機能を実装
+ * @param {string} theme
+ * 選択されたテーマをローカルストレージにセットする処理
  */
-function saveSelectTheme() {}
+function setToggleTheme(nextTheme) {
+  localStorage.setItem(TOGGLE_THEME_STORAGE_KEY, nextTheme);
+}
+/**
+ * TODO セットした値も文字列型だからパースなしでOK
+ * TODO ページ読み込みごとに実行する
+ * @returns {string}
+ */
+function getThemeSetting() {
+  const currentTheme = localStorage.getItem(TOGGLE_THEME_STORAGE_KEY);
+  if (currentTheme) {
+    console.log(currentTheme);
+    return currentTheme;
+  } else {
+    console.log("データがありません。");
+    return;
+  }
+}
